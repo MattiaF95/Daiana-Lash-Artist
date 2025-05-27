f2711155f1bbf4e256b08261a8dbf4b91273cc5c
MattiaF95 <fazi.mattia@gmail.com>
2025-05-27 14:32:56 +0200

    Modifica sezione portfoglio, riordinazione div per categoria e aggiunte foto daiana

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -1,14 +1,15 @@
    
    // Widget Simplibook per prenotazioni
-   (function(w, d, s, i) {
+    (function(w, d, s, i) {
         var script = d.createElement(s);
         script.async = true;
         script.src = "//widget.simplybook.it/v2/widget/widget.js";
         script.onload = function() {
-            new SimplybookWidget({"widget_type":"iframe","url":"https:\/\/daianalashartist.simplybook.it","theme":"minimal","theme_settings":{"timeline_show_end_time":"1","timeline_modern_display":"as_slots","hide_company_label":"0","timeline_hide_unavailable":"1","hide_past_days":"0","sb_base_color":"#5c3324","btn_color_1":"#5c3324,#6e3d2a,#99573d","link_color":"#5c3324","display_item_mode":"block","body_bg_color":"#f9f9f9","sb_review_image":"","dark_font_color":"#38160d","light_font_color":"#ffffff","sb_company_label_color":"#6e4230","hide_img_mode":"0","sb_busy":"#d9caca","sb_available":"#38160d"},"timeline":"modern","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":0,"allow_switch_to_ada":0,"predefined":[]},"container_id":"sbw_giwswo"});
+            new SimplybookWidget({"widget_type":"iframe","url":"https:\/\/daianalashartist.simplybook.it","theme":"minimal","theme_settings":{"timeline_show_end_time":"1","timeline_modern_display":"as_slots","hide_company_label":"0","timeline_hide_unavailable":"1","hide_past_days":"0","sb_base_color":"#5c3324","btn_color_1":"#5c3324,#6e3d2a,#99573d","link_color":"#5c3324","display_item_mode":"block","body_bg_color":"#f9f9f9","sb_review_image":"","dark_font_color":"#38160d","light_font_color":"#ffffff","sb_company_label_color":"#6e4230","hide_img_mode":"1","sb_busy":"#d9caca","sb_available":"#38160d"},"timeline":"modern","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":0,"allow_switch_to_ada":0,"predefined":[]},"container_id":"sbw_giwswo"});
         };
         d.head.appendChild(script);
     })(window, document, 'script', 'sbw_giwswo');
+
     // -----
 
     // Widget Simplibook per contatti
